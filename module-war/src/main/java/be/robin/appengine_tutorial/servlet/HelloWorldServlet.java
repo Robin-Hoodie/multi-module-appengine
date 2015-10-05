8971eaece76f36a2eb05b16b072ada3a4ac0317b
RobinHellemans <Robin.Hellemans@dhl.com>
2015-10-05 14:24:37 +0200

    Initial commit

---
+++ module-war/src/main/java/be/robin/appengine_tutorial/servlet/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package be.robin.appengine_tutorial.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by rohellem on 05/10/2015.
+ */
+public class HelloWorldServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		resp.setContentType("text/html");
+		writer.println("Hello world!");
+	}
+}
